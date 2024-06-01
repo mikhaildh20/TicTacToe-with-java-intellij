3dc77ed5ee663254cb7de6358f4079d101ddc597
Herdiansah <herdianah@gail.com>
2024-06-02 01:00:12 +0700

    push

---
+++ src/Board.java
@@ -0,0 +1,262 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Board extends JFrame{
+    private JPanel TicTacBoard;
+    private JButton R1C1;
+    private JButton R3C3;
+    private JButton R1C2;
+    private JButton R1C3;
+    private JButton R2C1;
+    private JButton R2C2;
+    private JButton R2C3;
+    private JButton R3C1;
+    private JButton R3C2;
+    private JLabel playerTurn;
+    private JLabel WinnerLabel;
+    private JButton resetButton;
+
+    int player = 1;
+    int filled = 0;
+
+    TicTac game = new TicTac();
+
+    Board(){
+        loadValue();
+        playerTurn.setText(String.valueOf(player));
+        setContentPane(TicTacBoard);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        R1C1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(0,player);
+                R1C1.setText(setButton());
+                R1C1.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R1C2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(1,player);
+                R1C2.setText(setButton());
+                R1C2.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R1C3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(2,player);
+                R1C3.setText(setButton());
+                R1C3.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R2C1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(3,player);
+                R2C1.setText(setButton());
+                R2C1.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R2C2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(4,player);
+                R2C2.setText(setButton());
+                R2C2.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R2C3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(5,player);
+                R2C3.setText(setButton());
+                R2C3.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R3C1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(6,player);
+                R3C1.setText(setButton());
+                R3C1.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R3C2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(7,player);
+                R3C2.setText(setButton());
+                R3C2.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        R3C3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled++;
+                game.setIndex(8,player);
+                R3C3.setText(setButton());
+                R3C3.setEnabled(false);
+                checkColumn();
+                gameOver();
+                switchPlayer();
+                playerTurn.setText(String.valueOf(player));
+            }
+        });
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                filled=0;
+                clearButton();
+                clearValue();
+                WinnerLabel.setText("Tic Tac Toe");
+                player = 1;
+                playerTurn.setText(String.valueOf(player));
+                enableAllButton();
+                loadValue();
+            }
+        });
+    }
+
+    public void checkColumn(){
+        if (game.linedHorizontal() != 0 || game.linedVertical() != 0){
+            disableAllButton();
+            setLabel("Player "+player+" Wins!");
+        }
+    }
+
+    public void gameOver(){
+        if (isAllFilled() && game.linedHorizontal() == 0 || isAllFilled() && game.linedVertical() == 0){
+            disableAllButton();
+            setLabel("Tie!");
+        }
+    }
+
+    public void checkAllFilledWinner(){
+
+    }
+
+    public String setButton(){
+        if (player == 1){
+            return "X";
+        }
+        return "O";
+    }
+
+    public void setLabel(String text){
+        WinnerLabel.setText(text);
+    }
+
+    public void switchPlayer(){
+        if (player == 1){
+            player = 2;
+            return;
+        }
+        player = 1;
+    }
+
+    public boolean isAllFilled(){
+        if (filled == 9) return true;
+        return false;
+    }
+
+    public void disableAllButton(){
+        R1C1.setEnabled(false);
+        R1C2.setEnabled(false);
+        R1C3.setEnabled(false);
+        R2C1.setEnabled(false);
+        R2C2.setEnabled(false);
+        R2C3.setEnabled(false);
+        R3C1.setEnabled(false);
+        R3C2.setEnabled(false);
+        R3C3.setEnabled(false);
+    }
+
+    public void enableAllButton(){
+        R1C1.setEnabled(true);
+        R1C2.setEnabled(true);
+        R1C3.setEnabled(true);
+        R2C1.setEnabled(true);
+        R2C2.setEnabled(true);
+        R2C3.setEnabled(true);
+        R3C1.setEnabled(true);
+        R3C2.setEnabled(true);
+        R3C3.setEnabled(true);
+    }
+
+    public void clearButton(){
+        R1C1.setText("");
+        R1C2.setText("");
+        R1C3.setText("");
+        R2C1.setText("");
+        R2C2.setText("");
+        R2C3.setText("");
+        R3C1.setText("");
+        R3C2.setText("");
+        R3C3.setText("");
+    }
+
+    public void clearValue(){
+        game.clear();
+    }
+
+    public void loadValue(){
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+        game.insert(0);
+    }
+
+    public static void main(String[] args) {
+        Board b = new Board();
+    }
+}
---
+++ src/TicTac.java
@@ -0,0 +1,84 @@
+public class TicTac {
+    Node head;
+    int[] box;
+    int size;
+    int output;
+    private final static int MAX_SIZE = 9;
+    private final static  int row = 3;
+    private final static int column = 3;
+    public TicTac(){
+        this.head = null;
+        this.size = MAX_SIZE;
+    }
+
+    public void insert(int val){
+        Node node = new Node(val);
+        if (head == null){
+            head = node;
+            return;
+        }
+        node.next = head;
+        head = node;
+    }
+
+    public void setIndex(int idx, int val){
+        Node temp = head;
+        for (int i = 0;i<MAX_SIZE;i++){
+            if (i == idx){
+                temp.value = val;
+                break;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public int linedHorizontal(){
+        box = new int[3];
+        Node temp = head;
+        output = 0;
+        for (int i = 0;i < row;i++)
+        {
+            for (int j=0;j < column;j++)
+            {
+                box[j] = temp.value;
+                temp = temp.next;
+            }
+            if (box[0] == box[1] && box[1] == box[2] && box[0] != 0){
+                output = box[0];
+                break;
+            }
+        }
+        return output;
+    }
+
+    public int linedVertical(){
+        box = new int[9];
+        Node temp = head;
+        output = 0;
+        for (int i = 0;i<MAX_SIZE;i++){
+            box[i] = temp.value;
+            temp = temp.next;
+        }
+        for (int i = 0;i<column;i++){
+            if (box[i] == box[i+3] && box[i+3] == box[i+6] && box[i] != 0){
+                output = box[i];
+                break;
+            }
+        }
+        return output;
+    }
+
+    public void clear(){
+        head = null;
+    }
+
+    public static class Node{
+        Node next;
+        int value;
+
+        public Node(int value){
+            this.next = null;
+            this.value = value;
+        }
+    }
+}
